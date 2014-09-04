b3d1aa5ee1797d2c5f0d15fd4cd450c9e01c0942
van.zheng <van.zheng@ef.com>
2014-09-04 16:32:50 +0800

    check in the trimFormValues.js

---
+++ trimFormValues.js
@@ -0,0 +1,13 @@
+﻿(function ($) {
+    $.fn.trimFormValues = function () {
+        return this.each(function () {
+            var me = $(this);
+            var inputs = me.find('input, select').not(':button').not(':submit');
+
+            inputs.each(function () {
+                var input = $(this);
+                input.val($.trim(input.val()));
+            });
+        });
+    };
+})(jQuery);
